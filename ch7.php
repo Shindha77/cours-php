0ea6cb8495b0bae96829e6908450e7db792c5572
Jérémy Chalumeau <jchalumeau77@gmail.com>
2022-02-09 21:08:17 +0100

    challenge 8

---
+++ ch7.php
@@ -0,0 +1,22 @@
+<?php
+
+$characters = [
+  "Negan" => [
+      "city" => "The Sanctuary",
+      "weapon" => "Lucille"
+  ],
+  "Daryl" => [
+      "city" => "Alexandria",
+      "weapon" => "crossbow"
+  ],
+  "Ezekiel" => [
+      "city" => "The Kingdom",
+      "weapon" => "Shiva"
+  ],
+  "Jérémy" => [
+    "city" => "Fontainebleau",
+    "weapon" => "shotgun"
+  ]
+];
+
+var_dump($characters);
