c40cf483e4a0c7d1df97ad919eb4a56bc469ba66
Jérémy Chalumeau <jchalumeau77@gmail.com>
2022-02-20 17:27:09 +0100

    challenge 10 avec partie 2

--- challenge10/cart.php
+++ challenge10/cart.php
@@ -1,17 +1,37 @@
 <?php
 session_start();
-$title = 'Mon panier';
+$titre = 'Mon panier';
 require 'header.php';
 
-if(empty($_SESSION['name']))
-{
-  header("location: login.php");
-  exit();
+if (empty($_SESSION['name'])) {
+    header('location: login.php');
+    exit();
 }
+$total = 0;
+$ids = array_keys($_SESSION['cart']);
+if (empty($ids)) {
+    $products = array();
+} else {
+    $products = $pdo->query('SELECT livre.*, auteur.nom, auteur.prenom FROM livre JOIN auteur ON livre.auteur_id = auteur.id WHERE livre.id IN (' . implode(',', $ids) . ')');
+    $products = $products->fetchAll(PDO::FETCH_OBJ);
+}
+
+if (isset($_GET['del'])) {
+    unset($_SESSION['cart'][$_GET['del']]);
+    header('location: cart.php');
+}
+
+foreach ($products as $product) :
 ?>
+    <h1>Mon Panier</h1>
+    <p>Total du panier : <?= $product->prix; ?> €</p>
 
-<h2>Mon panier</h2>
+    <h2><?= $product->titre; ?></h2>
+    <p><?= $product->prenom . ' ' . $product->nom; ?></p>
+    <p>Prix unitaire : <?= $product->prix; ?> €</p>
+    <p>Quantité : <?= $_SESSION['cart'][$product->id]; ?></p>
+    <button type="button" name="del"><a href="cart.php?del=<?= $product->id; ?>"> Supprimer du panier</a></button>
 
-
+<?php endforeach; ?>
 <?php
 require 'footer.php';
--- challenge10/login.php
+++ challenge10/login.php
@@ -1,31 +1,26 @@
 <?php
 session_start();
-$title = 'Login';
+$titre = 'Login';
 require 'header.php';
 
-if(isset($_POST['name']))
-{
-  $_SESSION['name'] = $_POST['name'];
-} else 
-{
-  $_SESSION['name'] = '';
+if (isset($_POST['name'])) {
+    $_SESSION['name'] = $_POST['name'];
 }
 
-if(isset($_POST['validate']))
-  if(isset($_POST['name']) && !empty($_POST['name']))
-  {
-    header("location: index.php");
-  }
+if (isset($_POST['validate']))
+    if (!empty($_POST['name'])) {
+        header("location: index.php");
+    }
 ?>
 
 <form action="" method="post">
-  <div>
-    <label for="name">Nom :</label>
-    <input type="text" id="name" name="name" required>
-  </div>
-  <div class="button">
-    <button type="submit" name="validate">S'identifier</button>
-  </div>
+    <div>
+        <label for="name">Nom :</label>
+        <input type="text" id="name" name="name" required>
+    </div>
+    <div class="button">
+        <button type="submit" name="validate">S'identifier</button>
+    </div>
 </form>
 
 
